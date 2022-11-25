85873039bfc77526255a33ef33905b6bf17c7f1e
semen.jons <semen.jons@yandex.ru>
2022-11-25 17:06:02 +0300

    массив

---
+++ summa/Program.cs
@@ -0,0 +1,14 @@
+﻿ //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Write("введите число ");
+ int number = int.Parse(Console.ReadLine());
+ int sum = 0;
+    while (number > 0)
+    {
+        sum  = sum+ number % 10;
+         number /=10; 
+    }
+Console.WriteLine("сумма равна {0}", sum);
